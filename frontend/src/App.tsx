ab22083ea0c8af19a29b744b5ffef239080fc868
hojun <ghwnsdl999@naver.com>
2023-02-20 15:55:42 +0900

    프론트 초기 설정

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
