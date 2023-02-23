60b2521ca36df2edc1a9cdef49efdb558bbea36c
soojin <ksgf988@naver.com>
2023-02-23 10:17:49 +0900

    [BE] setting: 환경 세팅

---
+++ backend/src/main/java/com/baedal/monolithic/global/config/ModelMapperConfig.java
@@ -0,0 +1,22 @@
+package com.baedal.monolithic.global.config;
+
+import jdk.dynalink.linker.MethodTypeConversionStrategy;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper mapper = new ModelMapper();
+        mapper
+                .getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return mapper;
+    }
+
+}
