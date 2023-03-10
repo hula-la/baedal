7eadad1debcc271e1df831c29bdb3c59586a6d5a
soojin <ksgf988@naver.com>
2023-03-10 12:51:29 +0900

    [BE] fix: CORS 설정 변경

--- backend/src/main/java/com/baedal/monolithic/global/config/SecurityConfig.java
+++ backend/src/main/java/com/baedal/monolithic/global/config/SecurityConfig.java
@@ -74,8 +74,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","*"));
+        configuration.addAllowedHeader("*");
         configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
