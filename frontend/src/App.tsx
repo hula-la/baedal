b971b5de6631de1fd72a3df798304cea36a92b56
오하민 <ohm55545@naver.com>
2023-03-17 09:28:10 +0900

    [#44][FE] design-FE/main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import React, { lazy } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login/loginPage";
+import { CheckLogin } from "./pages/login/checkLogin";
 import RedirectPage from "./pages/login/redirectPage";
+import AddressPage from "./pages/addressPage/addressPage";
 import BasketPage from "./pages/basket/basketPage";
 import OrderPage from "./pages/order/orderPage";
 import StoreListPage from "./pages/storeList/storeListPage";
@@ -19,16 +21,19 @@ function App() {
       <nav>네브바</nav>
       <Router>
         <Routes>
-          <Route path="" element={<Home />} />
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/oauth2/redirect" element={<RedirectPage />}></Route>
-          <Route path="/basket" element={<BasketPage />}></Route>
-          <Route path="/order" element={<OrderPage />}></Route>
-          <Route path="/store" element={<StoreListPage />}></Route>
-          <Route path="/storeDetail" element={<StoreDetailPage />}></Route>
-          <Route path="/storeDetail/:id" element={<MenuDetail />}></Route>
-          <Route path="/review" element={<MakeReviewPage />}></Route>
-          <Route path="/myPage/*" element={<MyPage />}></Route>
+          <Route element={<CheckLogin />}>
+            <Route path="" element={<Home />} />
+            <Route path="/address" element={<AddressPage />}></Route>
+            <Route path="/basket" element={<BasketPage />}></Route>
+            <Route path="/order" element={<OrderPage />}></Route>
+            <Route path="/store" element={<StoreListPage />}></Route>
+            <Route path="/storeDetail" element={<StoreDetailPage />}></Route>
+            <Route path="/storeDetail/:id" element={<MenuDetail />}></Route>
+            <Route path="/review" element={<MakeReviewPage />}></Route>
+            <Route path="/myPage/*" element={<MyPage />}></Route>
+          </Route>
         </Routes>
       </Router>
     </div>
